c75539081c2ed919ed26e8ce8189b71f526b9ae4
zhangzhongguo <zhangzhongguo@58.com>
2022-06-13 16:07:48 +0800

    feat

---
+++ src/main/java/demo/HashMapDemo.java
@@ -0,0 +1,16 @@
+package demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zhangzhongguo
+ * @date 2022/06/13
+ */
+public class HashMapDemo {
+
+    public static void main(String[] args) {
+        Map map=new HashMap();
+    }
+
+}
---
+++ src/main/java/demo/Test.java
@@ -0,0 +1,16 @@
+package demo;
+
+
+/**
+ * @author zhangzhongguo
+ * @date 2022/06/09
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello!!!!");
+        String extPath = System.getProperty("java.ext.dirs");
+        System.out.println(System.getProperty("java.ext.dirs"));
+    }
+}
